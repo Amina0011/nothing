d349beb6bc3c2747d5790cf8d20baaf694c1d8de
Amina <157822543+Amina0011@users.noreply.github.com>
2024-08-13 02:21:03 +0500

    Add files via upload

---
+++ description (2).sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Function to pause and display messages with a delay
+pause() {
+    sleep 1
+}
+
+# Print the challenge description with pauses
+echo "How naive the staff are to leave sensitive information in plain text!"
+pause
+echo "Keep in mind, in Linux filesystem everything starts with what? ... Good!"
+pause
+echo
+echo "Hint: you may want to check .txt, .ini, .rtf, .html, .xml file formats"
+pause
+echo
+echo "Good luck!"
+
